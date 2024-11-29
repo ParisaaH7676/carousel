4852cc190e92a1dddeffd3ab648ce1c612297b77
ParisaaH7676 <114694428+ParisaaH7676@users.noreply.github.com>
2024-11-29 15:38:21 +0330

    Add files via upload

---
+++ DaisyUi/customers-comment/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
